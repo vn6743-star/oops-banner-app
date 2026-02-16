f91f3f9399c88e7fa802f423742f07a8649dcb35
vn6743-star <vn6743@srmist.edu.in>
2026-02-16 13:43:13 +0530

    UC2-BannerOOPS

---
+++ UC2OOPS.java
@@ -0,0 +1,12 @@
+/** UC2 - Banner OOPS*/
+public class OOPS {
+    public static void main(String[] args) {
+	System.out.println("  ***   " + "  ***** " + "  ***** " + "  ***** ");
+    System.out.println(" ** **  " + "  **  **" + "  **  **" + "  **    ");
+    System.out.println("**   ** " + "  **  **" + "  **  **" + "  **    ");
+    System.out.println("**   ** " + "  ***** " + "  ***** " + "  ***** ");
+    System.out.println("**   ** " + "  **    " + "  **    " + "      **");
+    System.out.println(" ** **  " + "  **    " + "  **    " + "  **  **");
+    System.out.println("  ***   " + "  **    " + "  **    " + "  ***** ");
+   }
+}
